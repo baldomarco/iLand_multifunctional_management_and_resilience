df1b0f5bdf8e89e16dd69fe9c300c310394d26ea
Dobor Laura <dobor@fld.czu.cz>
2026-05-04 15:05:52 +0200

    Plot absolute values for manuscript, impact, recovery, auc

---
+++ 2_Analyses_figures/2f_absolute_values_and_CCeffects_impact_recovery_auc.r
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------------------------2025/2026
+# Laura Dobor, CZU, dobor@fld.czu.cz
+# Study: iLand modeling management and resilience on Kostelec area
+# 2026.04.20.
+#
+#     ANALYSES AND VISUALIZATION
+#
+#
+#----------------------------------------------------------------------------------
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(gridExtra)   
+
+
+
+
+
+setwd("D:/___PROJECTS/2025_iLand_management_study/04_work/3_analyses/")
+
+dataroot<-"Output_summary_tables/"
+plotroot<-"Figures/"
+
+
+version<-"DISASTER2"
+date<-"2025-04-18"
+damage.all<-read.csv(paste0(dataroot,date,"_damages_DISASTER2.csv"))
+recovery.all<-read.csv( paste0(dataroot,date,"_recovery_DISASTER2.csv"))
+
